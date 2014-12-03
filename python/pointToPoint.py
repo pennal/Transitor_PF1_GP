e3d33bdb9ddcbaf8166adca3b778543502f6d507
Lucas Pennati <pennal@usi.ch>
2014-12-03 15:12:08 +0100

    Reorganisation of files

--- python/pointToPoint.py
+++ python/pointToPoint.py
@@ -1,45 +1,6 @@
-# -*- coding: utf-8 -*-
-import requests         # Used to request the data from the APIs
-import json             # Used to pretty print the incoming JSON data
-
-# Base URL for the API
-apiURL = 'http://transport.opendata.ch/v1/'
-
-def doRequest(urlForQuery):
-    """
-    Perform the actual HTTP request. Kept in a separate function to handle all exceptions
-    :param urlForQuery: URL for the query
-    :return: JSON formatted data
-    """
-    try:
-        #Actual http request
-        response = requests.get(urlForQuery)
-    except ConnectionError:
-        print("Not connected to the internet")
-    except:
-        print("An unknown error has occured")
-
-    # Convert incoming data to JSON, and return it
-    return json.loads(response.text)
+import common
 
 
-def getCorrectLocation(userInput):
-    """
-    Gets the correct station, based on the users input. Will always return the one with the highest score
-    :param userInput: Name of the station entered by the user
-    :return: (string) Name of the correct station
-    """
-    locationToCheck = userInput.replace(" ","%20")
-
-    # Convert incoming data to JSON
-    data = doRequest(apiURL + 'locations?query=' + locationToCheck)
-    stringToReturn = str(data["stations"][0]["name"])
-
-
-    return stringToReturn
-
-def getCorrectLocationURLFormatted(userInput):
-    return getCorrectLocation(userInput).replace("ü","ue").replace("ä","ae").replace("ö","oe").replace(" ","%20")
 
 def getConnectionsPointToPoint(stationFrom,stationTo,via = None,date=None, time=None,isArrivalTime = None,transportations=None,limit=None,direct=None,sleeper=None,couchette=None,bike=None):
     """
@@ -58,7 +19,8 @@ def getConnectionsPointToPoint(stationFrom,stationTo,via = None,date=None, time=
     :param bike: [OPTIONAL] whether bikes have to be allowed on the train               {0/1}
     :return: JSON data of the connections
     """
-    urlForRequest = apiURL + "connections"
+
+    urlForRequest = common.apiURL + "connections"
     # Get all input elements
     inputElements = locals()
     # Transform in a list
@@ -73,20 +35,17 @@ def getConnectionsPointToPoint(stationFrom,stationTo,via = None,date=None, time=
             finalValues.append([inputElementsKeys[i],inputElementsValues[i]])
 
     # We start by adding the departure station and the destination
-    urlForRequest += "?from=" + getCorrectLocationURLFormatted(stationFrom) + "&to=" + getCorrectLocationURLFormatted(stationTo)
+    urlForRequest += "?from=" + common.getCorrectLocationURLFormatted(stationFrom) + "&to=" + common.getCorrectLocationURLFormatted(stationTo)
 
     for i in range(0,len(finalValues)):
         if finalValues[i][0] == "via":
-            urlForRequest += "&" + str(finalValues[i][0]) + "=" + getCorrectLocationURLFormatted(str(finalValues[i][1]))
+            urlForRequest += "&" + str(finalValues[i][0]) + "=" + common.getCorrectLocationURLFormatted(str(finalValues[i][1]))
         else:
             urlForRequest += "&" + str(finalValues[i][0]) + "=" + str(finalValues[i][1])
 
     # HTTP Request with the data needed
-    rawJSON = doRequest(urlForRequest)
+    rawJSON = common.doRequest(urlForRequest)
 
     # Do some additional processing
 
     return rawJSON
-
-
-print(json.dumps(getConnectionsPointToPoint("Lugano","Zürich HB"),indent=4))
