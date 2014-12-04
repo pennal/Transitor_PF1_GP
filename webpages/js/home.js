bada075e8b842819563dba3336b175aa2b546fec
Alexander North <anorth96@gmail.com>
2014-12-04 15:13:08 +0100

    commented js file

--- webpages/js/home.js
+++ webpages/js/home.js
@@ -1,20 +1,27 @@
 $('a.homepage').click(function(event){
+	//stop link from being followed
 	event.preventDefault();
 
+	// Make the 'a' element the same size as its child so that it forces the other blocks to stay in place
 	$(this).css({
 		width: $(this).children('div').outerWidth(true)+'px',
 		height: $(this).children('div').outerHeight(true)+'px',
 		float: 'left'
 	});
+
+	// Frequently referenced
 	var theLink = $(this).attr('href');
 	var overlayView = $('#overlayView');
 	var flipDiv = $(this).children('div');
 
+	// Save offset before changing anything, make sure the box starts in the right place
 	flipDivOffset = flipDiv.offset();
 	
+	// move the box to the overlay view and show
 	overlayView.append(flipDiv);
 	overlayView.removeClass('hidden');
 	
+	// Create a new stylesheet and position the box in its old location
 	addStylesheetRules([
 		['.box.flip, .box2.flip', // Also accepts a second argument as an array of arrays instead
 			['left', flipDivOffset.left+'px'],
@@ -23,14 +30,22 @@ $('a.homepage').click(function(event){
 	]);
 
 
+	// Start the flip
 	flipDiv.addClass('flip');
 
+	// Stop scrollbars from showing and scrolling
 	$('body').css('overflow', 'hidden');
+
+	// Parse the new background colour
 	var backgroundColor = flipDiv.children().children('.front').css('background-color');
+
+	// Set background colour of the box
 	flipDiv.css('background-color', backgroundColor);
+
+	// Hide front panel to show the back
 	flipDiv.children('.flipper').children('.front').css('visibility','hidden');
 
-
+	// After delay (same time as flipping animation duration+delay), set the body's new bgcolour, then get resource
 	setTimeout(function(){
 		setBGColour(backgroundColor);
 		requestThenFadeOut(theLink);
@@ -42,13 +57,22 @@ $('a.homepage').click(function(event){
 
 
 function requestThenFadeOut (theLink) {
+
+	// Often used
 	var overlayView = $('#overlayView');
+
+	// Start Ajax
 	sendAjaxRequest(theLink, function(data){
+		// Set contentDiv to new page
 		replaceHTMLOfElement ($('#contentDiv'), data);
+
+		// Start fadeOut of overlay
 		overlayView.addClass('fadeOut');
+
+		// Set the overflow of body to its previous value
 		$('body').css('overflow', 'initial');
 		
-		// After animations, clean up overlay
+		// After all animations have finished, reset the overlay
 		setTimeout(function(){
 			overlayView.addClass('hidden');
 			overlayView.removeClass('fadeOut');
