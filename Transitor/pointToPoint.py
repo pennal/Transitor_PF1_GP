bbad9facbbc5e6979da816c30516ae26a6a4e053
pennatil <pennatil@student.ethz.ch>
2014-12-08 22:51:54 +0100

    Multiple stops WIP

--- Transitor/pointToPoint.py
+++ Transitor/pointToPoint.py
@@ -3,6 +3,18 @@ import jinja2 #Used to substitute the tags in the html
 import os
 import json
 
+def getDateAntTimeSplit(inputString):
+    splitInputData = inputString.split("T")
+    tempData = splitInputData[0].split("-")
+    date = ""
+    for el in range(0,len(tempData)):
+        if el != 0:
+            date += "/"
+        date += tempData[len(tempData)-el-1]
+
+    time = splitInputData[1][:-8]
+    return time,date
+
 def returnHTMLTable(data):
     """
     DEBUG!!!!!!!!!!!!!!!!!!!
@@ -10,10 +22,54 @@ def returnHTMLTable(data):
     for i in range(0,len(data["connections"])):
         stationFrom = data["connections"][i]["from"]["station"]["name"]
         stationTo = data["connections"][i]["to"]["station"]["name"]
-        departureTime = data["connections"][i]["from"]["departure"]
-        arrivalTime = data["connections"][i]["to"]["arrival"]
+
+        departureTimeTemp = data["connections"][i]["from"]["departure"]
+        departureTime, departureDate = getDateAntTimeSplit(departureTimeTemp)
+
+
+        arrivalTimeTemp = data["connections"][i]["to"]["arrival"]
+        arrivalTime, arrivalDate = getDateAntTimeSplit(arrivalTimeTemp)
+
+
         departurePlatform = data["connections"][i]["from"]["platform"]
         arrivalPlatform = data["connections"][i]["to"]["platform"]
+
+        try:
+            transfersTag = ""
+            newData = data["connections"][i]["sections"]
+            print("Transfers len: ",len(newData))
+            # for transfer in range(1,len(newData)):
+            #     templateLoader = jinja2.FileSystemLoader( searchpath="/" )
+            #     #Get the current path of this file. From here, put togehter the path of the template file
+            #     basePath = os.path.dirname(os.path.abspath(__file__))
+            #     # An environment provides the data necessary to read and
+            #     #   parse our templates.  We pass in the loader object here.
+            #     templateEnv = jinja2.Environment( loader=templateLoader )
+            #
+            #     # This constant string specifies the template file we will use.
+            #     #TEMPLATE_FILE = basePath + "/JinjaTemplates/table.jinja"
+            #     TEMPLATE_FILE = basePath + "/JinjaTemplates/transfersTemplate.jinja"
+            #     # Read the template file using the environment object.
+            #     # This also constructs our Template object.
+            #     template = templateEnv.get_template( TEMPLATE_FILE )
+            #
+            #     # Specify any input variables to the template as a dictionary.
+            #     templateVars = { "intermediateStep" : intermediateStation,
+            #                      "arrivalTimeStep" : arrivalTimeStep,
+            #                      "arrivalPlatform" : arrivalPlatform
+            #     }
+            #
+            #     # Finally, process the template to produce our final text.
+            #     outputText = template.render( templateVars )
+            #     transfersTag += outputText
+        except:
+            transfersTag = ""
+
+
+
+
+
+
 
         templateLoader = jinja2.FileSystemLoader( searchpath="/" )
         #Get the current path of this file. From here, put togehter the path of the template file
@@ -31,8 +87,10 @@ def returnHTMLTable(data):
 
         # Specify any input variables to the template as a dictionary.
         templateVars = { "stationFrom" : stationFrom,
+                         "transfer" : transfersTag,
                          "stationTo" : stationTo,
                         "departureTime" : departureTime,
+                        "departureDate" : departureDate,
                         "arrivalTime" : arrivalTime,
                         "departurePlatform":departurePlatform,
                         "arrivalPlatform":arrivalPlatform}
