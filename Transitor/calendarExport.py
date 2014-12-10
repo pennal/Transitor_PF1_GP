1a16d73feaf42eca2f254e1b28d1ef020079cf5d
Lucas Pennati <pennal@usi.ch>
2014-12-10 16:23:09 +0100

    Calendar export prototype

---
+++ Transitor/calendarExport.py
@@ -0,0 +1,26 @@
+from icalendar import Calendar, Event
+from datetime import datetime
+import pytz
+import  os
+
+
+def downloadEventForCalendar(departureTime,arrivalTime,stationFrom,stationTo):
+    cal = Calendar()
+    cal.add('prodid', 'Transitor')
+    cal.add('version', 'Beta')
+    event = Event()
+
+    summaryString = stationFrom + " to " + stationTo
+
+
+
+    event.add('summary', 'Python meeting about calendaring')
+    event.add('dtstart', datetime(2005,4,4,8,0,0,tzinfo=pytz.utc))
+    event.add('dtend', datetime(2005,4,4,10,0,0,tzinfo=pytz.utc))
+    event.add('dtstamp', datetime(2005,4,4,0,10,0,tzinfo=pytz.utc))
+    event['uid'] = '20050115T101010/27346262376@mxm.dk'
+    event.add('priority', 5)
+    cal.add_component(event)
+
+
+    return cal.to_ical()
